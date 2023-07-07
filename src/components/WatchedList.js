fc5a69212675642f576a449b15b7613916a2728a
Stormblessed <penttilinkola10@gmail.com>
2023-07-07 18:17:09 +0300

    Pass onMovie prop in WatchedMovie

--- src/components/WatchedList.js
+++ src/components/WatchedList.js
@@ -1,7 +1,7 @@
 import { WatchedMovie } from "./WatchedMovie";
 
-export function WatchedList({ watched, onDelete }) {
-  return <ul className="list">
-    {watched.map((movie, i) => <WatchedMovie onDelete={onDelete} key={i} movie={movie} />)}
+export function WatchedList({ watched, onDelete, onMovie }) {
+  return <ul className="list list-movies">
+    {watched.map((movie, i) => <WatchedMovie onMovie={onMovie} onDelete={onDelete} key={i} movie={movie} />)}
   </ul>;
 }
