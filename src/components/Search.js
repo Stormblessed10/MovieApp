6916c67c490749a4801e7b683e4163aec75b9bab
Stormblessed <penttilinkola10@gmail.com>
2023-07-07 14:14:12 +0300

    Add useRef for focus on the search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,27 @@
+import { useEffect, useRef } from "react";
+
 export function Search({ query, setQuery }) {
+  const searchBar = useRef(null);
+  
+  useEffect(() => {
+    function callback (e) {
+      if (document.activeElement === searchBar.current) return;
+      if (e.key === "Enter") {
+        searchBar.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown", callback);
+    searchBar.current.focus();
+
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
   return <input
     className="search"
     type="text"
     placeholder="Search movies..."
     value={query}
-    onChange={(e) => setQuery(e.target.value)} />;
+    onChange={(e) => setQuery(e.target.value)} 
+    ref={searchBar}/>
 }
